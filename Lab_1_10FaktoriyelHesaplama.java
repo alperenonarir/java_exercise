67a67ceea296ed0ee0252c54ed65e68024b67898
alperenonarir <alperenonarir@gmail.com>
2023-01-12 16:50:18 +0300

    First Commit

---
+++ Lab_1_10FaktoriyelHesaplama.java
@@ -0,0 +1,21 @@
+// 1-) 10! Hesaplama
+
+
+
+
+public class Lab_1_10FaktoriyelHesaplama {
+
+    public static void main(String[] args) {
+        
+    
+        int x = 1;
+        for(int i=1;  i<=10; i++ ){
+            x = x*i;
+        }
+
+        System.out.println(x);
+
+
+    }
+    
+}
---
+++ Lab_2_Kelime_Alma_Yazdirma.java
@@ -0,0 +1,26 @@
+// 2-) Kelime alma ve yazdırma
+
+import java.util.Scanner;
+
+public class Lab_2_Kelime_Alma_Yazdirma {
+
+    public static void main(String[] args) {
+        
+        String isim_1,isim_2,isim_3;
+    
+        Scanner sc = new Scanner(System.in);
+       
+   
+       System.out.println("Arka arkaya 3 kelime giriniz");
+   
+       isim_1 = sc.next();
+       isim_2 = sc.next();
+       isim_3 = sc.next();
+   
+       System.out.println(isim_1 + " " + isim_2 + " " + isim_3);
+
+     sc.close();
+
+    }
+  
+}
